2cd11626c15feb1a0ab22234747dfea8a7d5dbdf
elkayy <63310939+imelkayy@users.noreply.github.com>
2024-02-15 10:58:11 -0800

    Studio 6

--- studio_6/1_guided_lesson.py
+++ studio_6/1_guided_lesson.py
@@ -10,24 +10,24 @@ student_1_major = "Computer Science"
 student_1_expected_graduation_year = "2026"
 
 student_2_id = 18582185
-student_2_name = "Jennie"
+student_2_first_name = "Jennie"
 student_2_last_name = "Kim"
 student_2_major = "Chemistry"
 student_2_expected_graduation_year = "2025"
 
 # TODO: Create a new student student_3 following the same format
 
-
-
-
+student_3_id = 2343524
+student_3_first_name = "Eleana"
+student_3_last_name = "Cherry"
+student_3_major = "Biology"
+student_3_expected_graduation_year = "2028"
 
 # Discussion: What are some problems with this approach?
 
-
-
-
-
-
+"""
+It requires lots of redundancy per-person, cannot be automated easily, creates lots of parameters
+"""
 
 # OOP Property - Encapsulation: Let's contain all of this data into a single Student class
 
@@ -35,42 +35,50 @@ student_2_expected_graduation_year = "2025"
 # Basic structure of a class
 class Student:
     # TODO: Let's build this class!
-    def __init__(self):
-        pass
+    def __init__(self, id, first_name, last_name, major, graduation_year):
+        self.__id = id
+        self.first_name = first_name
+        self.last_name = last_name
+        self.major = major
+        self.graduation_year = graduation_year
 
-
+    @property
+    def id(self):
+        return self.__id
+    
+    def get_last_four(self):
+        return str(self.__id)[-4:]
+    
+    def print_degree_title(self):
+        print("Bachelor of {major}".format(major=self.major))
 
 # TODO: Let's recreate our 3 students as objects of our new class!
 
-
-
-
-
-
+student_1 = Student(18584831, "Daniel", "White", "Computer Science", 2026)
+student_2 = Student(18582185, "Jennie", "Kim", "Chemistry", 2025)
+student_3 = Student(2343524, "Eleana", "Cherry", "Biology", 2028)
 
 # OOP Property Abstraction: The idea of making certain data private
 # This prevents other functions/parts of the code from accessing or modifying a value.
     
-# TODO: Make the id private.
+# DONE: Make the id private.
 # Test your code and ensure you cannot access student_1.__id (you should see an error)
-    
-
 
 
 # This is good because it prevents users from modifying the id by accident.
 # However, what if they need to see the id?
     
-# TODO: Add an @property getter for id
+# DONE: Add an @property getter for id
 # Test to make sure you can get the id with student_1.id
     
-
+print(student_2.id)
 
 # What if we want a way to just get the last four of the id instead of the whole thing? We can build a custom class method to do this.
     
 # Create a method in the class called get_last_four
 # This should return the last four numbers of the id.
     
-
+print("Last four",student_2.get_last_four())
 
 # OOP Property - Inheritence:
 # A class can inherit from another class.
@@ -78,18 +86,29 @@ class Student:
 # The class inherited from is known as the parent class
     
 # TODO: Create a child class called GradStudent which inherits from the Student class, with the additional property of "specialization"
+class GradStudent(Student):
+    def __init__(self, id, first_name, last_name, major, graduation_year, specialization):
+        super().__init__(id, first_name, last_name, major, graduation_year)
+        self.specialization = specialization
 
+    def print_degree_title(self):
+        print("Masters of {major} with a specialization in {spec}.".format(major=self.major, spec=self.specialization))
 
 # create a new student_4 which uses GradStudent instead.
 # this person's major is Computer Science and their Specialization is Artifical Intelligence
-    
+
+student_4 = GradStudent(81234112,"Luke","Tucker","Computer Science",2091,"Artifical Intelligence")
 
 # OOP Property - Polymorphism
 # refers to methods/functions/operators with the same name that can be executed on many objects or classes.
     
 # for example, maybe we want a method called print_degree_title, which will print "Bachelor of {major}".
 # TODO: add the print_degree_title method to the Student class
-    
+
+student_1.print_degree_title()
+student_2.print_degree_title()
+student_3.print_degree_title()
+
 # What happens if we run student_4.print_degree_title?
 # Yes, it will print "Bachelor of Computer Science".
     
@@ -99,6 +118,8 @@ class Student:
     
 # TODO: Add the print_degree_title to the Graduate class, have it print "Master of {major} with a specialization in {specialization}."
     
+student_4.print_degree_title()
+
 # Test your code and make sure print_degree_title prints the master's student as intended.
     
 # This is the end of the OOP lesson. Look at part_2.py for the follow up to this lesson. 
--- studio_6/2_studio_6_classwork.py
+++ studio_6/2_studio_6_classwork.py
@@ -5,18 +5,50 @@ class Vehicle:
         self.max_speed = max_speed
         self.mileage = mileage
 
-# TODO: Create a child class Bus that will inherit all of the variables and methods of the Vehicle class.
 
-# TODO: Instantiate your Bus class to an object to a variable called school_bus, the max speed 80, and mileage 45000.
+# DONE: Create a child class Bus that will inherit all of the variables and methods of the Vehicle class.
+        
+class Bus(Vehicle):
+    def __init__(self, max_speed, mileage, seating_capacity=4):
+        super().__init__(max_speed, mileage)
+        self.seating_capacity = seating_capacity
 
-# TODO: Add seating_capacity as a parameter in the def __init__(), but give it a default property of 4 seating_capacity=4 and add self.seating_capacity = seating_capacity.
+    def __str__(self):
+        return "This bus has a max speed of {speed:.0f}, {miles:.0f} mileage, and a seating capacity of {seats:.0f}.".format(speed=self.max_speed, miles=self.mileage, seats=self.seating_capacity)
 
-# TODO: Create a method in your Bus class called set_seating_capacity, which takes the parameter: seating_capacity and sets self.seating_capacity to equal the seating_capacity parameter.
+    def set_seating_capacity(self, num):
+        self.seating_capacity = num
 
-# TODO: call school_bus.set_seating_capacity and pass a value of 42.
+# DONE: Instantiate your Bus class to an object to a variable called school_bus, the max speed 80, and mileage 45000.
 
-# TODO: Override the __str__ function in the Bus class to print all of the properties onto a single line.
+school_bus = Bus(80, 45000)
+school_bus.set_seating_capacity(42)
+print(school_bus)
 
-# TODO: Create a new class which will also inherit the Vehicle class (a different type of vehicle than a bus). Inherit everything from vehicle and add one more set_xxxx method to set a property of that vehicle. 
+# DONE: Add seating_capacity as a parameter in the def __init__(), but give it a default property of 4 seating_capacity=4 and add self.seating_capacity = seating_capacity.
+
+# DONE: Create a method in your Bus class called set_seating_capacity, which takes the parameter: seating_capacity and sets self.seating_capacity to equal the seating_capacity parameter.
+
+# DONE: call school_bus.set_seating_capacity and pass a value of 42.
+
+# DONE: Override the __str__ function in the Bus class to print all of the properties onto a single line.
+
+# DONE: Create a new class which will also inherit the Vehicle class (a different type of vehicle than a bus). Inherit everything from vehicle and add one more set_xxxx method to set a property of that vehicle. 
+
+class Car(Vehicle):
+    def __init__(self, max_speed, mileage, model="Volvo"):
+        super().__init__(max_speed, mileage)
+        self.model = model
+
+    def __str__(self):
+        return "This car is a {model}, with a max speed of {speed:.0f} and {miles:.0f} miles!".format(model=self.model,speed=self.max_speed,miles=self.mileage)
+    
+    def set_model(self, model):
+        self.model = model
+
+car = Car(214, 3123)
+print(car)
+car.set_model("BMW")
+print(car)
 
 # TODO: Push both new files to your remote Engr102 repository and copy and paste your repository URL as a submission.
